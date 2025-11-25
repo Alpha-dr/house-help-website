6ff32c083d69048c521a3b8cdb72550453a61acb
Alpha-dr <81736447+Alpha-dr@users.noreply.github.com>
2025-11-25 17:32:49 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbPvByCy4wMu2BAoKYlQqpgSeaIBTYJSo",
   authDomain: "house-help-d4a29.firebaseapp.com",
   projectId: "house-help-d4a29",
-  storageBucket: "house-help-d4a29.firebasestorage.app",
+  storageBucket: "house-help-d4a29.appspot.com",  // FIXED ✔
   messagingSenderId: "541067969082",
   appId: "1:541067969082:web:eec4d9e43dc4e2395d6b03"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
